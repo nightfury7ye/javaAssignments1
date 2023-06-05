4cce3dcd55704472eb334e958f834136a441c019
Praveen Vemula <praveenvemula@Pavans-MacBook-Air.local>
2023-06-05 20:29:12 +0530

    2nd commit

---
+++ src/com/techlabs/iterativestatements/PrimeNumOrNot.java
@@ -0,0 +1,25 @@
+package com.techlabs.iterativestatements;
+
+import java.util.Scanner;
+
+public class PrimeNumOrNot {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("enter a number:");
+		int number = scanner.nextInt();
+	    boolean flag = false;
+	    for (int i = 2; i <= number / 2; ++i) {
+	      if (number % i == 0) {
+	        flag = true;
+	        break;
+	      }
+	    }
+	    if (!flag)
+	      System.out.println(number + " is a prime number.");
+	    else
+	      System.out.println(number + " is not a prime number.");
+	    scanner.close();
+	}
+
+}
